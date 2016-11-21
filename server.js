d672f455aa5a54d01c22698f57c93c520a740ab0
Gabor Rajczi <gabor.rajczi@gmail.com>
2016-11-21 12:50:51 +0000

    first solution

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.get("/", parseHeader);
+
+app.listen(port, running);
+
+function running(){
+    console.log("App is running on port "+port+"!");
+}
+
+function parseHeader(req, res){
+    var ip = req.ip.split(":").pop();
+    var acceptLanguage = req.headers["accept-language"].split(",")[0];
+    var userAgent = req.headers['user-agent'].split("(")[1].split(")")[0];
+    var result = {
+        ipaddress: ip,
+        language: acceptLanguage,
+        software: userAgent
+    }
+    res.send(result);
+}
